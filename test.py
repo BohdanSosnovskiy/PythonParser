e0de7a44df5262bf31734375453f62648484d4e7
BohdanSosnovskiy <40169231+BohdanSosnovskiy@users.noreply.github.com>
2019-03-15 16:32:24 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ def get_html(url):
     r = requests.get(url)
     return r.text #return HTML-code page (url)
 
+#parse all vacancy
 def get_items(html):
     soup = BeautifulSoup(html, 'lxml')
 
@@ -47,7 +48,6 @@ def create_connection(db_file):
         conn.close()
 
 def sql_base():
-    #create_connection('/home/alex/Рабочий стол/work/siteWorkNest.db')
     conn = sqlite3.connect("siteWorkNest.db") # или :memory: чтобы сохранить в RAM
     cursor = conn.cursor()
 
@@ -56,6 +56,7 @@ def sql_base():
                       (Title text, City text, Offer text,
                        About_project text)
                    """)
+
 def add_row(title,city,offer,about):
     conn = sqlite3.connect("siteWorkNest.db") # или :memory: чтобы сохранить в RAM
     cursor = conn.cursor()
@@ -83,16 +84,66 @@ def clear_table():
     sql = "DELETE FROM Jobs"
     cursor.execute(sql)
 
-def main():
+def view_vacance(selCity):
     ements = get_items(get_html('https://www.work-nest.com/jobs/'))
-    clear_table()
-    for i in ements:
-        print('Job: ' + i.Title)
-        print('City: ' + i.City)
-        print('We are ready to offer: ' + i.Offer)
-        print('About the project: ' + i.About_project)
-        add_row(i.Title,i.City,i.Offer,i.About_project)
-    view_base()
+    if selCity == 'all':
+        for i in ements:
+            print('Job: ' + i.Title)
+            print('City: ' + i.City)
+            print('We are ready to offer: ' + i.Offer)
+            print('About the project: ' + i.About_project)
+    else:
+        for i in ements:
+            if i.City.find(selCity) > -1:
+                print('Job: ' + i.Title)
+                print('City: ' + i.City)
+                print('We are ready to offer: ' + i.Offer)
+                print('About the project: ' + i.About_project)
+
+
+def main():
+    isWork = True
+    while isWork:
+        print('1. View all vacancy')
+        print('2. View vacancy Kharkiv')
+        print('3. View vacancy Kyiv')
+        print('4. View vacancy Remote')
+        print('5. Write all vacancy in SQLite')
+        choice = input('Select option:')
+
+        if choice == '1' or choice == 1:
+            view_vacance('all')
+        else:
+            if choice == '2' or choice == 2:
+                view_vacance('Kharkiv')
+            else:
+                if choice == '3' or choice == 3:
+                    view_vacance('Kyiv')
+                else:
+                    if choice == '4' or choice == 4:
+                        view_vacance('Remote')
+                    else:
+                        if choice == '5' or choice == 5:
+                            ements = get_items(get_html('https://www.work-nest.com/jobs/'))
+                            clear_table()
+                            for i in ements:
+                                add_row(i.Title,i.City,i.Offer,i.About_project)
+
+        choice = input('Exit [y/n]: ')
+        if choice == 'y':
+            isWork = False
+        pass
+
+
+    #ements = get_items(get_html('https://www.work-nest.com/jobs/'))
+    #clear_table()
+    #for i in ements:
+    #    print('Job: ' + i.Title)
+    #    print('City: ' + i.City)
+    #    print('We are ready to offer: ' + i.Offer)
+    #    print('About the project: ' + i.About_project)
+    #    add_row(i.Title,i.City,i.Offer,i.About_project)
+    #view_base()
 
 
 if __name__ == '__main__':
