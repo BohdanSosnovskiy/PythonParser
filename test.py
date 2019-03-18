2f9ead41fda83ec66808722ac289a3282854c498
BohdanSosnovskiy <40169231+BohdanSosnovskiy@users.noreply.github.com>
2019-03-18 17:29:36 +0300

    Added arguments

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
+import sys
 import sqlite3
 from sqlite3 import Error
 import os.path
+import requests
+from bs4 import BeautifulSoup
+
 
 DB_NAME = 'parser.db'
 LINK = 'https://www.work-nest.com/jobs/'
 
-class Job:
+
+class Parser:
 
     def __init__(self, db_name):
         self.conn = sqlite3.connect(db_name)
@@ -19,44 +22,87 @@ class Job:
             'create table vacancy '
             '(title text, city text, offer text, about text);'
         )
-        
-    def parse(self, careers_url, view=None):
+
+    def parse(self, careers_url, view = False, filter_city = None):
         r = requests.get(careers_url)
         soup = BeautifulSoup(r.text, 'html.parser')
 
         elements = soup.find_all('section', class_='opening-section-2')
 
         for name in elements:
-            title = name.find('h3',class_='section-title').get_text()
+            if filter_city is None:
+                title = name.find('h3',class_='section-title').get_text()
 
-            city = name.find('div',class_='opening-info-item-details').get_text()
+                city = name.find('div',class_='opening-info-item-details').get_text()
 
-            offer = name.find('div',class_='col-md-auto').find('ul').get_text()
+                offer = name.find('div',class_='col-md-auto').find('ul').get_text()
 
-            p = name.find('div',class_='col-md-auto').find('p')
-            if p:
-                about = p.get_text()
+                p = name.find('div',class_='col-md-auto').find('p')
+                if p:
+                    about = p.get_text()
+                else:
+                    about = ''
+
+                if view:
+                    print('Title: ' + title)
+                    print('City: ' + city)
+                    print('Offer: ' + offer)
+                    print('About: ' + about)
+                    print('_______________________________________________________')
+
+                c = self.conn.cursor()
+                c.execute(
+                    'insert into vacancy (title, city, offer, about) '
+                    'values (?, ?, ?, ?)',
+                    (title, city, offer, about)
+                )
             else:
-                about = ''
+                temp = name.find('div',class_='opening-info-item-details').get_text()
 
-            if ((view is not None) and (view == True)):
-                print('Title: ' + title)
-                print('City: ' + city)
-                print('Offer: ' + offer)
-                print('About: ' + about)
-                print('_______________________________________________________')
+                if temp.find(filter_city) > -1:
 
-            c = self.conn.cursor()
-            c.execute(
-                'insert into vacancy (title, city, offer, about) '
-                'values (?, ?, ?, ?)',
-                (title, city, offer, about)
-            )
+                    title = name.find('h3',class_='section-title').get_text()
+
+                    city = name.find('div',class_='opening-info-item-details').get_text()
+
+                    offer = name.find('div',class_='col-md-auto').find('ul').get_text()
+
+                    p = name.find('div',class_='col-md-auto').find('p')
+                    if p:
+                        about = p.get_text()
+                    else:
+                        about = ''
+
+                    if view:
+                        print('Title: ' + title)
+                        print('City: ' + city)
+                        print('Offer: ' + offer)
+                        print('About: ' + about)
+                        print('_______________________________________________________')
+
+                    c = self.conn.cursor()
+                    c.execute(
+                        'insert into vacancy (title, city, offer, about) '
+                        'values (?, ?, ?, ?)',
+                        (title, city, offer, about)
+                    )
             self.conn.commit()
 
 def main():
-    job = Job(db_name=DB_NAME)
-    job.parse(careers_url=LINK,view=True)
+
+    is_view = False
+    sel_city = None
+
+    if (len(sys.argv) == 2) and (sys.argv[1] == '--print'):
+        is_view = True
+    elif len(sys.argv) == 3:
+        sel_city = sys.argv[2]
+        is_view = True
+    elif len(sys.argv) == 2 and (sys.argv[1] != '--print'):
+        sel_city = sys.argv[1]
+
+    job = Parser(db_name = DB_NAME)
+    job.parse(careers_url = LINK, view = is_view, filter_city = sel_city)
 
 if __name__ == '__main__':
     main()
